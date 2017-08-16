d4caede242261132e8101754c5de5d93f7af7c14
Arash Hatami <hatamiarash7@gmail.com>
2017-08-16 16:51:50 +0430

    add mobile for authors list

--- resources/views/admin/authors.blade.php
+++ resources/views/admin/authors.blade.php
@@ -6,7 +6,7 @@
 
 @section('list')
     <div class="row">
-        <div class="col-lg-6 col-centered center-block" style="float: none;">
+        <div class="col-lg-9 col-centered center-block" style="float: none;">
             <div class="box box-primary">
                 <div class="box-header with-border">
                     <h2 class="box-title">Last Authors</h2>
@@ -24,7 +24,8 @@
                                     <thead>
                                     <tr>
                                         <th style="font-size:17px; text-align: right; direction: rtl;">آدرس</th>
-                                        <th style="font-size:17px; text-align: right; direction: rtl;">شماره تماس</th>
+                                        <th style="font-size:17px; text-align: right; direction: rtl;">تلفن ثابت</th>
+                                        <th style="font-size:17px; text-align: right; direction: rtl;">موبایل</th>
                                         <th style="font-size:17px; text-align: right; direction: rtl;">نام</th>
                                     </tr>
                                     </thead>
@@ -33,6 +34,7 @@
                                         <tr>
                                             <td style="text-align: right; direction: rtl;">{{ $author->address }}</td>
                                             <td style="text-align: right; direction: rtl;">{{ $author->phone }}</td>
+                                            <td style="text-align: right; direction: rtl;">{{ $author->mobile }}</td>
                                             <td style="text-align: right; direction: rtl;"><a
                                                         href="{{ url('/admin/authors/'.$author->unique_id) }}">{{ $author->name }}</a>
                                         </tr>
