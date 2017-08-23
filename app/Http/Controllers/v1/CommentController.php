895421e07d425351ed9e17d5c42056c5cc8e0109
Arash Hatami <hatamiarash7@gmail.com>
2017-08-23 17:54:26 +0430

    complete send comment api

--- app/Http/Controllers/v1/CommentController.php
+++ app/Http/Controllers/v1/CommentController.php
@@ -5,11 +5,41 @@ namespace App\Http\Controllers\v1;
 use App\Http\Controllers\Controller;
 use App\Services\v1\CommentService;
 use Exception;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Log;
 
 class CommentController extends Controller
 {
+    protected $Comments;
 
+    /**
+     * ProductController constructor.
+     * @param CommentService $service
+     */
+    public function __construct(CommentService $service)
+    {
+        require(app_path() . '/Common/jdf.php');
+        $this->Comments = $service;
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $result = $this->Comments->createComment($request);
+            if ($result)
+                return response()->json([
+                    'error' => false
+                ], 201);
+            else
+                return response()->json([
+                    'error' => true
+                ], 201);
+
+        } catch (Exception $e) {
+            return response()->json([
+                'error' => true,
+                'error_msg' => $e->getMessage()
+            ], 500);
+        }
+    }
 }
