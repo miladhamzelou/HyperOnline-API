fd417d44e527b4298979bdaad5c0e22bfa787ed7
Arash Hatami <hatamiarash7@gmail.com>
2017-09-04 17:56:13 +0430

    change search panel

--- resources/views/admin/layouts/base.blade.php
+++ resources/views/admin/layouts/base.blade.php
@@ -288,19 +288,22 @@
                     <div class="input-group">
                         <div class="input-group-btn search-panel">
                             <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-                                <span id="search_concept">Filter by</span> <span class="caret"></span>
+                                <span id="search_concept">فیلتر</span> <span class="caret"></span>
                             </button>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="#contains">Contains</a></li>
-                                <li><a href="#its_equal">It's equal</a></li>
-                                <li><a href="#greather_than">Greather than ></a></li>
-                                <li><a href="#less_than">Less than < </a></li>
-                                <li class="divider"></li>
-                                <li><a href="#all">Anything</a></li>
+                            <ul class="dropdown-menu" role="menu" style="text-align: center">
+                                <li><a href="#products">کالا</a></li>
+                                <li><a href="#category1">دسته بندی مرحله اول</a></li>
+                                <li><a href="#category2">دسته بندی مرحله دوم</a></li>
+                                <li><a href="#category3">دسته بندی مرحله سوم</a></li>
+                                <li><a href="#user">کاربران</a></li>
+                                <li><a href="#orders">سفارشات</a></li>
+                                <li><a href="#comments">نظرات</a></li>
+                                <li><a href="#authors">فروشنده ها</a></li>
+                                <li><a href="#sellers">فروشگاه ها</a></li>
                             </ul>
                         </div>
                         <input type="hidden" name="search_param" value="all" id="search_param">
-                        <input type="text" class="form-control" name="x" placeholder="Search term...">
+                        <input type="text" class="form-control" name="word" style="text-align: right; direction: rtl" placeholder="عبارت جستجو ...">
                         <span class="input-group-btn">
                     <button class="btn btn-default" type="button"><span
                                 class="glyphicon glyphicon-search"></span></button>
@@ -386,6 +389,8 @@
 <script src="{{ asset('bower_components/fastclick/lib/fastclick.js') }}"></script>
 <script src="{{ asset('bower_components/jquery-sparkline/dist/jquery.sparkline.min.js') }}"></script>
 <script src="{{ asset('bower_components/jquery-slimscroll/jquery.slimscroll.min.js') }}"></script>
+
+<script src="{{ asset('js/mine.js') }}"></script>
 
 <!-- Optionally, you can add Slimscroll and FastClick plugins.
      Both of these plugins are recommended to enhance the
