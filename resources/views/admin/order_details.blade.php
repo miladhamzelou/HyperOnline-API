01fc15a6f58426546e3437072065ebceb9fbb1c2
Arash Hatami <hatamiarash7@gmail.com>
2017-09-16 03:34:38 +0430

    fix status show for order detail

--- resources/views/admin/order_details.blade.php
+++ resources/views/admin/order_details.blade.php
@@ -22,10 +22,18 @@
                         <p style="text-align: justify; direction: rtl;"><strong style="color:#3c8dbc; font-size: 19px;">نام مشتری : </strong>{{ $order->user_name }}</p><br>
                         <p style="text-align: justify; direction: rtl;"><strong style="color:#3c8dbc; font-size: 19px;">تلفن مشتری : </strong>{{ $order->user_phone }}</p><br>
                         <p style="text-align: justify; direction: rtl;"><strong style="color:#3c8dbc; font-size: 19px;">کالا ها : </strong>{{ $order->stuffs }}</p><br>
-                        <p style="text-align: justify; direction: rtl;"><strong style="color:#3c8dbc; font-size: 19px;">قیمت : </strong>{{ $order->price }}</p><br>
+                        <p style="text-align: justify; direction: rtl;"><strong style="color:#3c8dbc; font-size: 19px;">قیمت : </strong>{{ $order->price . ' تومان' }}</p><br>
                         <p style="text-align: justify; direction: rtl;"><strong style="color:#3c8dbc; font-size: 19px;">ساعت تحویل : </strong>{{ $order->hour }}</p><br>
                         <p style="text-align: justify; direction: rtl;"><strong style="color:#3c8dbc; font-size: 19px;">روش پرداخت : </strong>{{ $order->pay_method }}</p><br>
-                        <p style="text-align: justify; direction: rtl;"><strong style="color:#3c8dbc; font-size: 19px;">وضعیت فعلی : </strong>{{ $order->status }}</p><br>
+                        @if($order->status=="abort")
+                            <p style="text-align: justify; direction: rtl; color: red;"><strong style="color:#3c8dbc; font-size: 19px;">وضعیت فعلی : </strong>لغو شده</p><br>
+                        @elseif($order->status=="delivered")
+                            <p style="text-align: justify; direction: rtl; color: green;"><strong style="color:#3c8dbc; font-size: 19px;">وضعیت فعلی : </strong>تحویل شده</p><br>
+                        @elseif($order->status=="shipped")
+                            <p style="text-align: justify; direction: rtl; color: darkviolet"><strong style="color:#3c8dbc; font-size: 19px;">وضعیت فعلی : </strong>ارسال شده</p><br>
+                        @elseif($order->status=="pending")
+                            <p style="text-align: justify; direction: rtl; color: orange"><strong style="color:#3c8dbc; font-size: 19px;">وضعیت فعلی : </strong>درحال انتظار</p><br>
+                        @endif
                         <p style="text-align: justify; direction: rtl;"><strong style="color:#3c8dbc; font-size: 19px;">کد : </strong>{{ $order->code }}</p><br>
                         <p style="text-align: justify; direction: rtl;"><strong style="color:#3c8dbc; font-size: 19px;">توضیحات : </strong>{{ $order->description }}</p><br>
                         <p style="text-align: right; direction: ltr"><strong style="color:#3c8dbc; font-size: 19px;">تاریخ / ساعت : </strong>{{ $order->create_date }}</p><br>
