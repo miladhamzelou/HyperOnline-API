d3b14f7e52852f10e4039194a1990080931fe714
Arash Hatami <hatamiarash7@gmail.com>
2017-09-15 19:49:43 +0430

    fix link for income sms

--- routes/web.php
+++ routes/web.php
@@ -88,5 +88,5 @@ Route::get('/search', 'v1_web\AdminController@search');
 
 Route::get('category/{level}/{id}','v1\market\CategoryController@index');
 
-Route::post('downloadLink', 'v1\MainController@downloadLink');
-Route::get('downloadLink', 'v1\MainController@downloadLink');
+Route::post('/downloadLink', 'v1\MainController@downloadLink');
+Route::get('/downloadLink', 'v1\MainController@downloadLink');
