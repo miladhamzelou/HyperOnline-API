385cc235a43b2946a5dcda33c8d6ece82c682f96
Arash Hatami <hatamiarash7@gmail.com>
2017-09-21 00:28:01 +0430

    complete add to cart

---
+++ config/cart.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'tax' => 0,
+    'database' => [
+        'connection' => null,
+        'table' => 'shoppingcart',
+    ],
+    'destroy_on_logout' => false,
+    'format' => [
+        'decimals' => 0,
+        'decimal_point' => '.',
+        'thousand_seperator' => ','
+    ],
+];
