c7d6b44677f59f0b907d21d24dc5fce68822f447
Arash Hatami <hatamiarash7@gmail.com>
2017-09-21 19:30:15 +0430

    add ltrim

--- app/Http/Controllers/v1_web/AdminController.php
+++ app/Http/Controllers/v1_web/AdminController.php
@@ -444,6 +444,8 @@ class AdminController
                 $total_price += $order->price;
                 $total_price_original += $order->price_original;
                 $total_send += $order->price_send;
+                $order->stuffs_count = ltrim($order->stuffs_count, ',');
+                Log::info($order->stuffs_count);
                 $count = explode(',', $order->stuffs_count);
                 foreach ($count as $i)
                     $total_count += $i;
