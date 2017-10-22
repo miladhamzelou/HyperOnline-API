3e4050ad1366a315c3d29cd132f19c6438471bdd
Arash Hatami <hatamiarash7@gmail.com>
2017-10-22 17:09:52 +0330

    fix hours

--- resources/views/admin/order_details.blade.php
+++ resources/views/admin/order_details.blade.php
@@ -32,8 +32,13 @@
                                 ها : </strong>{{ $order->stuffs }}</p><br>
                         <p style="text-align: justify; direction: rtl;"><strong style="color:#3c8dbc; font-size: 19px;">قیمت
                                 : </strong>{{ $order->price . ' تومان' }}</p><br>
-                        <p style="text-align: justify; direction: rtl;"><strong style="color:#3c8dbc; font-size: 19px;">ساعت
-                                تحویل : </strong>{{ $order->hour }}</p><br>
+                        @if($order->hour==18)
+                            <p style="text-align: justify; direction: rtl;"><strong style="color:#3c8dbc; font-size: 19px;">ساعت
+                                    تحویل : </strong> الی </p><br>
+                        @else
+                            <p style="text-align: justify; direction: rtl;"><strong style="color:#3c8dbc; font-size: 19px;">ساعت
+                                    تحویل : </strong>{{ $order->hour }}</p><br>
+                        @endif
                         <p style="text-align: justify; direction: rtl;"><strong style="color:#3c8dbc; font-size: 19px;">روش
                                 پرداخت : </strong>{{ $order->pay_method }}</p><br>
                         @if($order->status=="abort")
--- resources/views/emails/order.blade.php
+++ resources/views/emails/order.blade.php
@@ -6,7 +6,11 @@
 <br>
 <p style="direction: rtl;"><strong>محصولات</strong> : {{ $order['stuffs'] }}</p>
 <br>
-<p style="direction: rtl;"><strong>ساعت ارسال</strong> : {{ $order['hour'] }}</p>
+@if($order['hour']==18)
+    <p style="direction: rtl;"><strong>ساعت ارسال : </strong>18:30 الی 19:30</p>
+@else
+    <p style="direction: rtl;"><strong>ساعت ارسال</strong> : {{ $order['hour'] }}</p>
+@endif
 <p style="direction: rtl;"><strong>هزینه سبد خرید</strong> : {{ $order['price'].' تومان' }}</p>
 @if($order['desc'])
     <p style="direction: rtl;"><strong>توضیحات</strong> : {{ $order['desc'] }}</p>
