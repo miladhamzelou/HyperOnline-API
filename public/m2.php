52e7df13c88531a3b06f1fbd570fcf9623ebe5bc
Arash Hatami <hatamiarash7@gmail.com>
2018-01-28 16:03:19 +0330

    change mail php

---
+++ public/m2.php
@@ -0,0 +1,9 @@
+<?php
+$to = "hatamiarash7@gmail.com";      
+$subject = "Mail Test at ".strftime("%T", time());      
+$message = "This is a test."; 
+$from = "HyperOnline <info@hyper-online.ir>";
+$headers = "From: {$from}\r\n";
+$result = mail($to, $subject, $message, $header); 
+echo $result ? 'Sent' : 'Error';
+?>
