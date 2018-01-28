ec9852dc785e292194ec719a9846750f977ddf2b
Arash Hatami <hatamiarash7@gmail.com>
2018-01-29 01:51:43 +0330

    change logo

--- resources/views/market/logo.blade.php
+++ resources/views/market/logo.blade.php
@@ -1,6 +1,14 @@
 <html>
 <head>
     <style>
+        div {
+            max-width: 500px;
+        }
+
+        img {
+            width: 100%;
+        }
+
         .centered {
             position: fixed;
             top: 50%;
@@ -12,14 +20,12 @@
 </head>
 <body>
 <div class="centered">
-    <img src="{{ asset('/market/image/asnaf.png') }}"
+    <img id='sizpnbqesizpesgtfukz'
          onclick='window.open("https://logo.samandehi.ir/Verify.aspx?id=92906&p=pfvluiwkpfvlobpdgvka", "Popup","toolbar=no, scrollbars=no, location=no, statusbar=no, menubar=no, resizable=0, width=450, height=630, top=30")'
-
-    />
+         src='https://logo.samandehi.ir/logo.aspx?id=92906&p=bsiyodrfbsiylymawlbq'/>
     <br>
-    <img src="https://www.aparat.com/public/public/images/logo/v2/aparat_logo_fa_color_black_275x100.png"
+    <img src="https://trustseal.enamad.ir/logo.aspx?id=71394&amp;p=QHelLvhqUOh0EElo"
          onclick="window.open(&quot;https://trustseal.enamad.ir/Verify.aspx?id=71394&amp;p=QHelLvhqUOh0EElo&quot;, &quot;Popup&quot;,&quot;toolbar=no, location=no, statusbar=no, menubar=no, scrollbars=1, resizable=0, width=580, height=600, top=30&quot;)"
-
-    />
+         id="QHelLvhqUOh0EElo">
 </div>
 </body>
