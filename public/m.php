d6ab84f716a90b1a6a5250005a1670cb2f54ea9d
Arash Hatami <hatamiarash7@gmail.com>
2018-01-28 15:51:03 +0330

    change mail php

--- public/m.php
+++ public/m.php
@@ -1,6 +1,11 @@
 <?php
 // The message
-$message = "developer email : hatamiarash7@gmail.com\nApplication : HyperOnline - ir.hatamiarash.hyperonline";
+$message = "باسمه تعالی\nمدیریت محترم سایت آپارات\nاحتراما اینجانب امیر حاجی بابایی مدیر فروشگاه مجازی 'هایپر آنلاین' تقاضا دارم کانال ایجاد شده تحت عنوان هایپر آنلاین در سایت آپارات را به کانال رسمی تبدیل نمایید.
+لازم به ذکر است هایپر آنلاین اولین فروشگاه مجازی در استان همدان می باشد دارای پروانه کسب از اتحادیه کشوری کسب و کار مجازی است.
+تصویر جواز کسب و همچنین فرم درخواست کانال رسمی برای شما ارسال میگردد.
+آدرس سایت:
+www.hyper-online.ir
+باتشکر فراوان";
 echo $message;
 // In case any of our lines are larger than 70 characters, we should use wordwrap()
 $message = wordwrap($message, 70, "\r\n");
