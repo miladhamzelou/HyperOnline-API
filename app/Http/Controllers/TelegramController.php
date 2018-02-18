51cd3bccd9d642e2d556f50b235e5933a4c32c3d
hatamiarash7 <hatamiarash7@gmail.com>
2018-02-19 01:28:50 +0330

    fix

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -24,7 +24,7 @@ class TelegramController extends Controller
 	{
 		$result = Telegram::setWebhook([
 			'url' => 'https://hyper-online.ir/' . config('telegram.bot_token') . '/webhook',
-			'certificate' => '/var/www/HyperOnline-API/private.key'
+			'certificate' => '/etc/nginx/ssl/bundle.crt'
 		]);
 		return $result;
 	}
