9d63a741252b8c428721ffd502a401a66c90debb
Arash Hatami <hatamiarash7@gmail.com>
2018-02-22 17:15:08 +0330

    fix problem

--- app/Http/Controllers/v1/OrderController.php
+++ app/Http/Controllers/v1/OrderController.php
@@ -436,8 +436,6 @@ class OrderController extends Controller
         $res = $this->verify($this->API, $pay->transId);
         $res = (array)json_decode($res);
 
-        return $res['status'];
-
         if ($res['status'] == 1) {
             $order = Order::where("unique_id", $pay->factorNumber)->firstOrFail();
             $order->temp = 0;
