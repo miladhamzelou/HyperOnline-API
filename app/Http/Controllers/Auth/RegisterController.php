5b4ea7f8bb330f7c5e031605d18ba2d5a6c84f4f
hatamiarash7 <hatamiarash7@gmail.com>
2018-04-03 17:15:44 +0430

    fix city and address
    Signed-off-by: hatamiarash7 <hatamiarash7@gmail.com>

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -9,116 +9,116 @@ use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Register Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users as well as their
-    | validation and creation. By default this controller uses a trait to
-    | provide this functionality without requiring any additional code.
-    |
-    */
+	/*
+	|--------------------------------------------------------------------------
+	| Register Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller handles the registration of new users as well as their
+	| validation and creation. By default this controller uses a trait to
+	| provide this functionality without requiring any additional code.
+	|
+	*/
 
-    use RegistersUsers;
+	use RegistersUsers;
 
-    /**
-     * Where to redirect users after registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
+	/**
+	 * Where to redirect users after registration.
+	 *
+	 * @var string
+	 */
+	protected $redirectTo = '/home';
 
-    /**
-     * Create a new controller instance.
-     *
-     */
-    public function __construct()
-    {
-        require(app_path() . '/Common/jdf.php');
-        $this->middleware('guest');
-    }
+	/**
+	 * Create a new controller instance.
+	 *
+	 */
+	public function __construct()
+	{
+		require(app_path() . '/Common/jdf.php');
+		$this->middleware('guest');
+	}
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => 'required|string|max:255',
-            'phone' => 'required|string|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
-        ]);
-    }
+	/**
+	 * Get a validator for an incoming registration request.
+	 *
+	 * @param  array $data
+	 * @return \Illuminate\Contracts\Validation\Validator
+	 */
+	protected function validator(array $data)
+	{
+		return Validator::make($data, [
+			'name' => 'required|string|max:255',
+			'phone' => 'required|string|max:255|unique:users',
+			'password' => 'required|string|min:6|confirmed',
+		]);
+	}
 
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array $data
-     * @return User
-     */
-    protected function create(array $data)
-    {
-        $count = count(User::get()) + 1;
-        $hash = $this->hashSSHA($data['password']);
+	/**
+	 * Create a new user instance after a valid registration.
+	 *
+	 * @param  array $data
+	 * @return User
+	 */
+	protected function create(array $data)
+	{
+		$count = count(User::get()) + 1;
+		$hash = $this->hashSSHA($data['password']);
 
-        return User::create([
-            'unique_id' => uniqid('', false),
-            'code' => "HO-" . $count,
-            'name' => $data['name'],
-            'phone' => $data['phone'],
-            'password' => bcrypt($data['password']),
-            'salt' => $hash["salt"],
-            'encrypted_password' => $hash["encrypted"],
-            'address' => "hamedan",
-            'state' => "hamedan",
-            'city' => "hamedan",
-            'create_date' => $this->getDate($this->getCurrentTime()) . ' ' . $this->getTime($this->getCurrentTime())
-        ]);
-    }
+		return User::create([
+			'unique_id' => uniqid('', false),
+			'code' => "HO-" . $count,
+			'name' => $data['name'],
+			'phone' => $data['phone'],
+			'password' => bcrypt($data['password']),
+			'salt' => $hash["salt"],
+			'encrypted_password' => $hash["encrypted"],
+			'address' => $data['address'],
+			'state' => "hamedan",
+			'city' => $data['city'],
+			'create_date' => $this->getDate($this->getCurrentTime()) . ' ' . $this->getTime($this->getCurrentTime())
+		]);
+	}
 
-    /**
-     * @param $password
-     * @return array
-     */
-    public function hashSSHA($password)
-    {
-        $salt = sha1(rand());
-        $salt = substr($salt, 0, 10);
-        $encrypted = base64_encode(sha1($password . $salt, true) . $salt);
-        $hash = array("salt" => $salt, "encrypted" => $encrypted);
-        return $hash;
-    }
+	/**
+	 * @param $password
+	 * @return array
+	 */
+	public function hashSSHA($password)
+	{
+		$salt = sha1(rand());
+		$salt = substr($salt, 0, 10);
+		$encrypted = base64_encode(sha1($password . $salt, true) . $salt);
+		$hash = array("salt" => $salt, "encrypted" => $encrypted);
+		return $hash;
+	}
 
-    protected function getCurrentTime()
-    {
-        $now = date("Y-m-d", time());
-        $time = date("H:i:s", time());
-        return $now . ' ' . $time;
-    }
+	protected function getCurrentTime()
+	{
+		$now = date("Y-m-d", time());
+		$time = date("H:i:s", time());
+		return $now . ' ' . $time;
+	}
 
-    protected function getDate($date)
-    {
-        $now = explode(' ', $date)[0];
-        $time = explode(' ', $date)[1];
-        list($year, $month, $day) = explode('-', $now);
-        list($hour, $minute, $second) = explode(':', $time);
-        $timestamp = mktime($hour, $minute, $second, $month, $day, $year);
-        $date = jDate("Y/m/d", $timestamp);
-        return $date;
-    }
+	protected function getDate($date)
+	{
+		$now = explode(' ', $date)[0];
+		$time = explode(' ', $date)[1];
+		list($year, $month, $day) = explode('-', $now);
+		list($hour, $minute, $second) = explode(':', $time);
+		$timestamp = mktime($hour, $minute, $second, $month, $day, $year);
+		$date = jDate("Y/m/d", $timestamp);
+		return $date;
+	}
 
-    protected function getTime($date)
-    {
-        $now = explode(' ', $date)[0];
-        $time = explode(' ', $date)[1];
-        list($year, $month, $day) = explode('-', $now);
-        list($hour, $minute, $second) = explode(':', $time);
-        $timestamp = mktime($hour, $minute, $second, $month, $day, $year);
-        $date = jDate("H:i", $timestamp);
-        return $date;
-    }
+	protected function getTime($date)
+	{
+		$now = explode(' ', $date)[0];
+		$time = explode(' ', $date)[1];
+		list($year, $month, $day) = explode('-', $now);
+		list($hour, $minute, $second) = explode(':', $time);
+		$timestamp = mktime($hour, $minute, $second, $month, $day, $year);
+		$date = jDate("H:i", $timestamp);
+		return $date;
+	}
 }
