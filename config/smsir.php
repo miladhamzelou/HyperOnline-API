7f3ef3f93e6dee6aaedef9432d4423dbd1d0825e
hatamiarash7 <hatamiarash7@gmail.com>
2018-04-26 15:48:06 +0430

    remove default values

--- config/smsir.php
+++ config/smsir.php
@@ -1,25 +1,21 @@
 <?php
 
 return [
-
 	/* Important Settings */
-
 	// ======================================================================
 	// never remove 'web', . just put your middleware like auth or admin (if you have) here. eg: ['web','auth']
 	'middlewares' => ['web'],
 	// you can change default route from sms-admin to anything you want
 	'route' => 'sms-admin',
 	// SMS.ir Api Key
-	'api-key' => env('SMSIR-API-KEY','587f547ced3bb9316ea5547c'),
+	'api-key' => env('SMSIR-API-KEY'),
 	// SMS.ir Secret Key
-	'secret-key' => env('SMSIR-SECRET-KEY','Mna32#%12Thp'),
+	'secret-key' => env('SMSIR-SECRET-KEY'),
 	// Your sms.ir line number
-	'line-number' => env('SMSIR-LINE-NUMBER','30004505004615'),
+	'line-number' => env('SMSIR-LINE-NUMBER'),
 	// ======================================================================
-
 	// set true if you want log to the database
 	'db-log' => true,
-
 	/* Admin Panel Title */
 	'title' => 'هایپرآنلاین',
 	// How many log you want to show in sms-admin panel ?
