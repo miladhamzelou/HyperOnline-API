81a2c0e2868433fa7dc663c19777787fb7d85e7d
hatamiarash7 <hatamiarash7@gmail.com>
2018-05-19 00:26:23 +0430

    add raychat

--- resources/views/market/layout/base.blade.php
+++ resources/views/market/layout/base.blade.php
@@ -40,7 +40,18 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('bower_components/sweetalert2/dist/sweetalert2.css') }}">
 @yield('style')
 <!-- CSS Part End-->
+    <script type="text/javascript">!function () {
+            function t() {
+                var t = document.createElement("script");
+                t.type = "text/javascript", t.async = !0, localStorage.getItem("rayToken") ? t.src = "https://app.raychat.io/scripts/js/" + o + "?rid=" + localStorage.getItem("rayToken") + "&href=" + window.location.href : t.src = "https://app.raychat.io/scripts/js/" + o;
+                var e = document.getElementsByTagName("script")[0];
+                e.parentNode.insertBefore(t, e)
+            }
 
+            var e = document, a = window, o = "67340b52-5eb5-4837-ad60-915e73f88596";
+            "complete" === e.readyState ? t() : a.attachEvent ? a.attachEvent("onload", t) : a.addEventListener("load", t, !1)
+        }();
+    </script>
 </head>
 <body>
 <div class="wrapper-wide">
