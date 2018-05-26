85e5cd42d9742d6cdfbe299dc1655e783d0b382e
hatamiarash7 <hatamiarash7@gmail.com>
2018-05-26 23:01:29 +0430

    remove user from output for relations

--- app/Wallet.php
+++ app/Wallet.php
@@ -9,9 +9,11 @@ class Wallet extends Model
 	protected $primaryKey = 'unique_id';
 	public $incrementing = false;
 
+	protected $hidden = ['user'];
+
 	public function user()
 	{
-		return $this->belongsTo(User::class);
+		return $this->belongsTo(User::class, 'user_id');
 	}
 
 	public function transactions()
