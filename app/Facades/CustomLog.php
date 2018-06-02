36e05476c8c4dccdde482bc7a517771f14423dda
hatamiarash7 <hatamiarash7@gmail.com>
2018-06-02 21:35:39 +0430

    some changes

--- app/Facades/CustomLog.php
+++ app/Facades/CustomLog.php
@@ -2,63 +2,66 @@
 /**
  * Copyright (c) 2017 - All Rights Reserved - Arash Hatami
  */
+
 namespace App\Facades;
+
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\Facades\Log;
-use Monolog\Handler\StreamHandler;
 use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 
 class CustomLog extends Facade
 {
-    /**
-     * Get Facade Accessor for Current Class
-     *
-     * @return string Accessor
-     */
-    protected static function getFacadeAccessor()
-    {
-        return 'customlog';
-    }
+	/**
+	 * Get Facade Accessor for Current Class
+	 *
+	 * @return string Accessor
+	 */
+	protected static function getFacadeAccessor()
+	{
+		return 'customlog';
+	}
 
-    /**
-     * Handle dynamic, static calls to the object.
-     *
-     * @param  string  $method
-     * @param  array   $args
-     * @return mixed
-     */
-    public static function __callStatic($method, $args)
-    {
-        if (! isset($args[0])) {
-            return;
-        }
+	/**
+	 * Handle dynamic, static calls to the object.
+	 *
+	 * @param  string $method
+	 * @param  array $args
+	 * @return mixed
+	 * @throws \Exception
+	 */
+	public static function __callStatic($method, $args)
+	{
+		if (!isset($args[0])) {
+			return;
+		}
 
-        $message = $args[0];
-        $file = isset($args[1]) ? $args[1] : 'laravel';
+		$message = $args[0];
+		$file = isset($args[1]) ? $args[1] : 'laravel';
 
-        // Maximum number of lines in one log file
-        $max = 10000;
+		// Maximum number of lines in one log file
+		$max = 10000;
 
-        // Get Monolog Instance
-        $logger = Log::getMonolog();
+		// Get Monolog Instance
+		$logger = Log::getMonolog();
 
-        // Trim log file to a max length
-        $path = storage_path('logs/'.$file.'.log');
-        if (! file_exists($path)) {
-            fopen($path, "w");
-        }
-        $lines = file($path);
-        if (count($lines) >= $max) {
-            file_put_contents($path, implode('', array_slice($lines, -$max, $max)));
-        }
+		// Trim log file to a max length
+		$path = storage_path('logs/' . $file . '.log');
+		if (!file_exists($path)) {
+			fopen($path, "w");
+		}
+		$lines = file($path);
+		if (count($lines) >= $max) {
+			file_put_contents($path, implode('', array_slice($lines, -$max, $max)));
+		}
 
-        // Define custom Monolog handler
-        $handler = new StreamHandler($path, Logger::DEBUG);
-        $handler->setFormatter(new LineFormatter(null, null, true, true));
+		// Define custom Monolog handler
+		$handler = new StreamHandler($path, Logger::DEBUG);
+		$handler->setFormatter(new LineFormatter(null, null, true, true));
 
-        // Set defined handler and log the message
-        $logger->setHandlers([$handler]);
-        $logger->$method($message);
-    }
+		// Set defined handler and log the message
+		$logger->setHandlers([$handler]);
+		$logger->$method($message);
+	}
 }
