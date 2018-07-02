721b399997bf48a7e1e90672b1c850e0d333421d
hatamiarash7 <hatamiarash7@gmail.com>
2018-07-02 14:55:15 +0430

    change style

--- resources/views/market/confirm.blade.php
+++ resources/views/market/confirm.blade.php
@@ -6,15 +6,18 @@
         <div class="col-md-8 col-md-offset-2">
             <table class="table table-bordered">
                 <tr>
-                    <td class="text-right" style="width: 20%; text-align: center; background-color: #eee;"><strong>نام</strong></td>
+                    <td class="text-right" style="width: 20%; text-align: center; background-color: #eee;">
+                        <strong>نام</strong></td>
                     <td class="text-right">{{ $data['user']['name'] }}</td>
                 </tr>
                 <tr>
-                    <td class="text-right" style="width: 20%; text-align: center; background-color: #eee;"><strong>شماره تماس</strong></td>
+                    <td class="text-right" style="width: 20%; text-align: center; background-color: #eee;"><strong>شماره
+                            تماس</strong></td>
                     <td class="text-right">{{ $data['user']['phone'] }}</td>
                 </tr>
                 <tr>
-                    <td class="text-right" style="width: 20%; text-align: center; background-color: #eee;"><strong>آدرس</strong></td>
+                    <td class="text-right" style="width: 20%; text-align: center; background-color: #eee;">
+                        <strong>آدرس</strong></td>
                     <td class="text-right">{{ $data['user']['address'] }}</td>
                 </tr>
             </table>
@@ -36,11 +39,11 @@
             <table class="table table-bordered">
                 <tr>
                     <td class="text-right" style="width: 10%; text-align: center; vertical-align: middle; !important;">
-                            <input title="gateway" type="radio" value="0" checked>
+                        <input title="gateway" type="radio" value="0" checked>
                     </td>
                     <td class="text-right" style="vertical-align: middle;">
                         <img src="http://www.sb24.com/dotAsset/1159138b-110a-4b8f-8d3f-363e4a5e805e.png"
-                        style="width: 50px;height: 50px;">
+                             style="width: 50px;height: 50px;">
                         پرداخت الکترونیک سامان
                     </td>
                 </tr>
@@ -58,12 +61,9 @@
             </table>
         </div>
     </div>
-    <div class="row">
-        <div class="col-md-6">
-            <a href="{{ url('website/pay') }}" class="btn btn-primary pull-left" style="font-size: 18px">پرداخت</a>
-        </div>
-        <div class="col-md-6">
-            <a href="{{ url('home') }}" class="btn btn-default" style="font-size: 18px">بازگشت</a>
+    <div class="buttons">
+        <div style="text-align: center">
+            <a href="{{ url('website/pay') }}" class="btn btn-primary" style="font-size: 18px;  border-radius: 20px">پرداخت</a>
         </div>
     </div>
 @endsection
